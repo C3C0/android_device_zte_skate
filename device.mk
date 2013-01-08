8ea2ffb98bd1ec472aa11e6aae24c05eda5f13f0
Peter Gregus <pete.gregus@gmail.com>
2013-01-08 18:32:59 +0100

    Build hwcomposer

--- device.mk
+++ device.mk
@@ -28,7 +28,8 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # Graphics
 PRODUCT_PACKAGES := \
         gralloc.msm7x27 \
-        copybit.msm7x27
+        copybit.msm7x27 \
+        hwcomposer.msm7x27
 
 # OMX
 PRODUCT_PACKAGES += \
